9d78134192c18c5266e599d2d5e6bb129e1f17f8
Alex Bender <axbndr@gmail.com>
2017-10-09 01:52:32 +0300

    Create base DB stub, create DumbDB

--- database.py
+++ database.py
@@ -1,3 +1,76 @@
+"""Database backends for holding notes"""
+from collections import defaultdict
+
+DUMB_TYPE = 'dumb'
+
 
 class DB():
-    pass
+    def __init__(self, db_type):
+        self.db_type = db_type
+
+    def create(self, path_hash, notes):
+        raise NotImplementedError
+
+    def read(self, path_hash):
+        raise NotImplementedError
+
+    def update(self, path_hash, note_id, text):
+        raise NotImplementedError
+
+    def delete(self, path_hash, note_id=None):
+        raise NotImplementedError
+
+    def backup(self):
+        raise NotImplementedError
+
+    def debug(self):
+        raise NotImplementedError
+
+
+class DumbDB(DB):
+
+    def __init__(self, db_type=DUMB_TYPE):
+        self.notes = defaultdict(list)
+        self.load()
+        super(DumbDB, self).__init__(db_type)
+
+    def create(self, path_hash, notes):
+        for note in notes:
+            self.notes[path_hash].append(note)
+        return True
+
+    def read(self, path_hash):
+        return self.notes.get(path_hash, None)
+
+    def update(self, path_hash, note_id, text):
+        if len(self.notes.get(path_hash, [])) > note_id-1:
+            self.notes[path_hash][note_id] = text
+            return True
+        else:
+            return False
+
+    def delete(self, path_hash, note_id=None):
+        if len(self.notes.get(path_hash, [])) > note_id-1:
+            res = self.notes.pop(note_id)
+            if res:
+                return True
+            else:
+                return False
+        return False
+
+    def dump(self):
+        storage_file = open('sorage.txt', 'w')
+
+        for path_hash in self.notes:
+            storage_file.write(path_hash+';'+','.join(self.notes[path_hash])+'\n')
+
+        storage_file.close()
+
+    def load(self):
+        storage_file = open('storage.txt', 'r')
+
+        for line in storage_file.readlines():
+            path_hash, notes = line.split(';')
+            self.notes[path_hash] = notes.split(',')
+
+        storage_file.close()
--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 Show directory specific notes.
 """
 import os
+import sys
+import hashlib
 import subprocess
-from database import DB
+from database import DumbDB
 
 
 CONFIG_PATH = '~/.config/dirnotes.ini'
@@ -14,27 +16,31 @@ DELIMETER = '\n' + '-' * 78 + '\n'
 
 def open_db():
     """Sqlite or json"""
-    db = DB()
+    db = DumbDB()
     return db
 
 
 def get_hash(path):
     "Calculate the hash of path (what about inode of the folder?)"
-    return 123
+    return hashlib.sha1(path.encode()).hexdigest()
 
 
-def get_notes(path):
-    path_hash = get_hash(path)
+def get_notes(db, path):
     "Notes lookup in the database (Sqlite?)"
-    return '1) This is a first note'
+    path_hash = get_hash(path)
+    return db.read(path_hash)
 
 
 def main():
-    out = subprocess.check_output(["ls", "-la"], universal_newlines=True)
-    db = open_db
-    notes = get_notes(os.path.abspath('.'))
+    args = sys.argv
+
+    out = subprocess.check_output(["ls"] + args[1:], universal_newlines=True)
+    db = open_db()
+    path = os.path.abspath('.')
+    notes = get_notes(db, path)
     print(out + DELIMETER)
-    print(notes)
+    for note in notes:
+        print(note.strip())
 
 
 if __name__ == "__main__":
